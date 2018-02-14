125702006d0e4ad2a22fcee1ba01ee394aa505c2
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-02-14 01:45:09 -0200

    allow default parameters to be overwritten

--- routes/beers.js
+++ routes/beers.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const axios = require('axios');
+const _ = require('lodash');
 
 const router = express.Router();
 const BEER_API = 'https://api.punkapi.com/v2/beers';
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  axios.get(`${BEER_API}/?abv_gt=1&abv_lt=10&ibu_gt=1&ibu_lt=1000&ebc_gt=1&ebc_lt=1000&beer_name=A&per_page=1&page=1`).then((response) => {
+  const defaultProperties = {
+    abv_gt: 1,
+    abv_lt: 10,
+    ibu_gt: 1,
+    ibu_lt: 1000,
+    ebc_gt: 1,
+    ebc_lt: 1000,
+    beer_name: 'A',
+    per_page: 1,
+    page: 1,
+  };
+  const allowedParameters = _.pick(req.query, Object.keys(defaultProperties));
+  const proxyProperties = Object.assign(defaultProperties, allowedParameters);
+
+  axios.get(`${BEER_API}`, proxyProperties).then((response) => {
     const { data } = response;
     return res.json(data);
   }).catch((error) => {
